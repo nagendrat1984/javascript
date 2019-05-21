391db03cf16dfacbf23d30d43cbdce2519d314df
Nagendra Tanguturi <nagendra_tanguturi_2@amp.com.au>
2019-05-21 15:20:07 +1000

    removed old code

--- test/login.js
+++ test/login.js
@@ -1,19 +1,11 @@
-//const assert = require('assert');
 import assert from 'assert';
-//let expect = require('chai').expect;
-import expect from 'chai'
+import expect from 'chai';
 
 // Import for custom commands
 import actions from '../customcommands/actions.js';
+//Import Pages
 import login_page from '../classes/login_page.js';
 import Page from '../classes/base_page.js';
-//import LoginPage from './classes/loginPage'
-
-// function compare(results){
-//     results.forEach((result, idx)=> expect(result.isExactSameImage, 'Image ' + idx + ' isn\'t the same').to.be.true);
-// }
-
-
 
 describe('webdriver.io page', () => {
 
@@ -22,15 +14,8 @@ describe('webdriver.io page', () => {
     });
    
     it('verify the title of the xplan page', () => {
-        
         const title = browser.getTitle();
         assert.equal(title, 'XPLAN Today | AMP XPLAN');
-        //browser.pause(30000);
-        // const userid = $('#userid');
-        // userid.setValue('ntanguturi');
-        // let report = browser.checkElement('.content');
-        // console.log(report);
-        // compare(report);
     });
 
     it('Enter login details', () => {
@@ -40,46 +25,18 @@ describe('webdriver.io page', () => {
 
         login_page.usernameInput.setValue('ipulikot');
         login_page.passwordInput.setValue('Password01');
-        login_page.compare('.content');
-
-        //const userid = $('#userid');
-        //userid.setValue('ipulikot');
-        //userid.setValue('ntanguturi');
-        //const password = $('#passwd');
-        //password.setValue('Password01')
-        //password.setValue('~Rishiv12012018')        
-        // let loginreport = browser.checkElement('.content');
-        // console.log(loginreport);
-        // compare(loginreport);
+        //login_page.compare('.content');
         
-  
     });
 
-    it('Verify login home page', () => {
-        
+    it('Verify login home page', () => {        
         login_page.loginButton.waitAndClick();
-        //login_page.compare('.framemain');
-
-        //const loginButton = $('#btn_login')
-        //loginButton.click();
-        //loginButton.waitAndClick();
-        //let homepagereport = browser.checkElement('.framemain');
-        //console.log(homepagereport);
-        //compare(homepagereport);         
+        //login_page.compare('.framemain');           
     });
 
 
     it('verify logout', () => {
-        
-        login_page.logout.waitAndClick();
-
-        // const logout = $('#mfts-logout');
-        // logout.click();
-        //const logouttext = $('#btn_login').getText();
-        //assert.equal(logouttext, 'Login');
-        //browser.pause(30000);       
+        login_page.logout.waitAndClick();    
     });
-
-
 
 });
